e49b7cd743a55a4c9966bb4aa06c7ca176f44551
Silas Hermann <s.hermann@billiger-mietwagen.de>
2024-07-29 18:53:57 +0200

    crate EnumBuilder to handle php enumerate

---
+++ src/ClassBuilder/EnumBuilder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Timelesstron\ObjectBuilder\ClassBuilder;
+
+use InvalidArgumentException;
+use ReflectionClass;
+
+class EnumBuilder implements ClassBuilderInterface
+{
+    /** @var array<string, string|array> */
+    private array $parameters;
+    private ReflectionClass $class;
+    public function build(ReflectionClass $class, array $parameters): object
+    {
+        $this->class = $class;
+        $this->parameters = $parameters;
+
+        return $this->generateRandomValue();
+    }
+
+    private function generateRandomValue(): object
+    {
+        $enums = array_values($this->class->getName()::cases());
+
+        if ($this->isValideParameter()) {
+            $enums = $this->parameters;
+            $enum = $enums[array_rand($enums)];
+
+            return constant(
+                sprintf(
+                    "%s::%s",
+                    $this->class->getName(),
+                    $enum
+                )
+            );
+        }
+
+        return $enums[array_rand($enums)];
+    }
+
+    private function isValideParameter(): bool
+    {
+        if (empty($this->parameters)) {
+
+            return false;
+        }
+
+        foreach ($this->parameters as $parameter) {
+            if (!in_array($parameter, array_column($this->class->getName()::cases(), 'value'))) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Invalid parameter given vor enum %s: "%s".',
+                        $this->class->getShortName(),
+                        implode(', ', $this->parameters)
+                    )
+                );
+            }
+        }
+
+        return true;
+    }
+}
--- src/Services/ClassBuilderService.php
+++ src/Services/ClassBuilderService.php
@@ -4,11 +4,14 @@ namespace Timelesstron\ObjectBuilder\Services;
 
 use ReflectionClass;
 use Timelesstron\ObjectBuilder\ClassBuilder\ClassBuilderInterface;
+use Timelesstron\ObjectBuilder\ClassBuilder\EnumBuilder;
 
 class ClassBuilderService
 {
     public static function getClassBuilder(ReflectionClass $reflection): ClassBuilderInterface
     {
-        // ToDo: Add Builders for all types of Classes
+         return match (true) {
+            $reflection->isEnum() => new EnumBuilder(),
+        };
     }
 }
